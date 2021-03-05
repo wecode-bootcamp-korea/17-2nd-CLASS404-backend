c4109432323cf23235b8b9c5748fd31878d2af82
ScottieKim <fmychioh@gmail.com>
2021-03-05 14:36:23 +0900

    MODIFY : tests.py 수정

---
+++ product/urls.py
@@ -0,0 +1,7 @@
+from django.urls   import path
+from product.views import ProductListView
+
+urlpatterns = [
+    path('', ProductListView.as_view()),
+
+]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+import json
+from django.db import reset_queries
 
-# Create your views here.
+from django.http            import JsonResponse, HttpResponse
+from django.views           import View
+from django.db.models       import Q, Count
+
+from user.utils     import login_decorator, login_check
+from .models import Brand, Category, Product, ProductUserlike, Review
+
+class ProductListView(View):
+    @login_check
+    def get(self, request):
+        user          = request.user
+
+        category_list = request.GET.getlist('category', None)
+        sort          = request.GET.get('sort', 'latestOrder')
+        
+        q = Q()
+        if category_list:
+            for category in category_list:
+                q.add(Q(category=category), q.OR)
+
+        sort_dict = {
+                'latestOrder'     : '-created_at',
+                'reviewCountOrder': '-num_reviews',
+                'popularOrder'    : '-like_product'
+        }
+
+        if sort in sort_dict:
+            products = Product.objects.filter(q)\
+                    .annotate(num_reviews=Count('review')).order_by(sort_dict[sort])
+        
+        product_info_list = [{
+            "id"          : product.id,
+            "thumbnail"   : product.thumbnail_url,
+            "likeCount"   : ProductUserlike.objects.filter(product_id=product.id).count(), 
+            "like"        : ProductUserlike.objects.filter(user_id=user.id, product_id=product.id).exists() if user else False,
+            "category"    : product.category.name,
+            "userName"    : product.user.name,
+            "title"       : product.title,
+            "price"       : product.price,
+	    "gift"        : product.gift,
+        } for product in products]
+        return JsonResponse({"product": product_info_list}, status=200)
+                
+
