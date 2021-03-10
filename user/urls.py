9e75499f83fcd4c321c4ae09ae8d1ded0051c0d7
namujinju <namujinju@gmail.com>
2021-03-10 12:16:22 +0900

    Modify: mypage 로직 및 unit test 수정 - mypage 메인을 like, buy, create한 클래스 모두 보여주게 수정 - user name, email을 전달하게 수정 - MYPAGE_LIMIT 변수로 메인에서 보여줄 클래스 수 지정

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views      import KakaoLoginView, MyPageView, SigninView
+from .views      import KakaoLoginView, MyPageView, SigninView, SignupView, MyPageView
 
 urlpatterns = [
     path('/signup', SignupView.as_view()),
