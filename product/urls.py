70af3cf39f00a705195b995dd39a6f43aafc906e
namujinju <namujinju@gmail.com>
2021-03-11 12:18:25 +0900

    Modify: product like view 추가, 불필요한 파일 삭제 - unit test, integration test 완료 - 불필요한 aws_s3.py, 연습용 utils.py 삭제

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,8 @@
 from django.urls   import path
-from product.views import ProductView, ProductDetailView, FileView
-
+from product.views import ProductView, ProductDetailView, LikeView
 
 urlpatterns = [
     path('', ProductView.as_view()),
     path('/<int:product_id>', ProductDetailView.as_view()),
-    path("/file", FileView.as_view()),
-
+    path('/<int:product_id>/like', LikeView.as_view()),
 ]
